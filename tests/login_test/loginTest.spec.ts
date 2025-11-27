6872e14e1f7e3cb90b288c223ab75e9106a5ad02
JohnAerisBarrot <johnaeris.barrot@eg-software.com>
2025-11-27 10:58:21 +0800

    [UPDATED] Added TS_02: Reset Password (but not fully working as of now since MailSlurp reached its limit); Added TS_03: Verify Broken Links

--- tests/login_test/loginTest.spec.ts
+++ tests/login_test/loginTest.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/login_module/loginPage';
-import { invalidData, sodexoLu } from '../../src/utils/environment';
+import { generic, invalidData, privacyPolicy, sodexoLu, termsOfService } from '../../src/utils/environment';
+import { SignUpPage } from '../../pages/signup_module/signUpPage';
+import { MailSlurpService } from '../../src/helpers/mailSlurpService';
+import { EmailVerificationPage } from '../../pages/signup_module/emailVerificationPage';
+import { EmailResetPasswordPage } from '../../pages/login_module/emailResetPasswordPage';
 
 test.describe('TS_01: Login Functionality', () => {
 
@@ -59,6 +63,55 @@ test.describe('TS_01: Login Functionality', () => {
 
 test.describe('TS_02: Reset Password', () => {
 
+  // test.beforeEach(async ({ page }) => {});
+
+  test('TS02_TC001: Reset password using registered email', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    const signUpPage = new SignUpPage(page);
+    const mail = new MailSlurpService();
+    const emailVerification = new EmailVerificationPage(page);
+    const emailResetPasswordPage = new EmailResetPasswordPage(page);
+
+    const inbox = await mail.createInbox();
+    console.log('Generated Email: ', inbox.emailAddress);
+    
+    await loginPage.goToLogin(`${generic.environment.stageURL}`);
+    
+    const popupSignUpPage = await signUpPage.goToSignUpPage();
+    await signUpPage.fillSignUpForm(popupSignUpPage, inbox.emailAddress);
+    
+    const verificationLink = await mail.waitForVerificationEmail(inbox.id);
+    console.log("Verification Link: ", verificationLink);
+    
+    await emailVerification.verifyEmail(popupSignUpPage, verificationLink, inbox.emailAddress);
+
+    await popupSignUpPage.getByRole('img', { name: 'menu' }).click();
+    await popupSignUpPage.getByText('Log Out').click();
+
+    await popupSignUpPage.getByRole('button', { name: 'Forgot your credentials?' }).click();
+
+    await popupSignUpPage.getByRole('textbox', { name: 'Email Address' }).fill(inbox.emailAddress);
+    await popupSignUpPage.getByRole('button', { name: 'RESET PASSWORD' }).click();
+    await popupSignUpPage.getByRole('button', { name: 'Okay' }).click();
+
+    const resetPasswordLink = await mail.waitForResetPasswordEmail(inbox.id);
+    console.log("Reset Password Link: ", resetPasswordLink);
+
+    const popupResetPasswordPage = await loginPage.goToResetPasswordPage(resetPasswordLink);
+    await emailResetPasswordPage.resetPassword(popupResetPasswordPage, inbox.emailAddress);
+
+  });
+
+  // test('TS02_TC002: Reset password using unregistered email', async ({ page }) => {});
+  // test('TS02_TC003: Reset password using invalid email', async ({ page }) => {});
+  // test('TS02_TC004: Reset password using registered email', async ({ page }) => {});
+  // test('TS02_TC005: Reset password using registered email', async ({ page }) => {});
+  // test.afterEach(async ({ page }) => { await page.close();});
+})
+
+test.describe('TS_03: Verify Broken Links', () => {
+
   let loginPage: LoginPage;
 
   test.beforeEach(async ({ page }) => {
@@ -66,24 +119,23 @@ test.describe('TS_02: Reset Password', () => {
     await loginPage.goToLogin(`${sodexoLu.environment.localURL}`);
   });
 
-  test('TS02_TC001: Reset password using registered email', async ({ page }) => {
-    // Empty Block
+  test('TS03_TC001: Verify if login page link is not broken', async ({ page }) => {
+    loginPage.assertVerifyURL(page, sodexoLu.environment.localURL);
   });
 
-  test('TS02_TC002: Reset password using unregistered email', async ({ page }) => {
-    // Empty Block
+  test('TS03_TC002: Verify if reset password link is not broken', async ({ page }) => {
+    await loginPage.clickResetPassword();
+    await loginPage.assertResetPasswordModalVisible();
   });
 
-  test('TS02_TC003: Reset password using invalid email', async ({ page }) => {
-    // Empty Block
+  test('TS03_TC003: Verify if terms of service link is not broken', async ({ page }) => {
+    const popup = await loginPage.clickTermsOfService();
+    await loginPage.assertVerifyURL(popup, termsOfService.link.enURL);
   });
 
-  test('TS02_TC004: Reset password using registered email', async ({ page }) => {
-    // Empty Block
-  });
-
-  test('TS02_TC005: Reset password using registered email', async ({ page }) => {
-    // Empty Block
+  test('TS03_TC004: Verify if privacy policy link is not broken', async ({ page }) => {
+    const popup = await loginPage.clickPrivacyPolicy();
+    await loginPage.assertVerifyURL(popup, privacyPolicy.link.enURL);
   });
 
   test.afterEach(async ({ page }) => {
@@ -91,8 +143,6 @@ test.describe('TS_02: Reset Password', () => {
   });
 
 })
-
-
 
 
 
