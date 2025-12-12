e7d3b0cbe130db52ae6062c62aa24d8c9ac882bd
JohnAerisBarrot <johnaeris.barrot@eg-software.com>
2025-12-12 14:57:07 +0800

    [UPDATED] Added comments

--- tests/signup_test/signUpTest.spec.ts
+++ tests/signup_test/signUpTest.spec.ts
@@ -8,24 +8,38 @@ import { EmailVerificationPage } from '../../pages/signup_module/emailVerificati
 
 test.describe('TS_01: Sign Up Functionality', () => {
 
+    let loginPage: LoginPage;
+    let signUpPage: SignUpPage;
+    let mail: MailSlurpService;
+    let emailVerification: EmailVerificationPage;
+
     test('[@generic][@regression] TS01_TC001: Sign up with valid email and valid password', async ({ page }) => {
 
-        const loginPage = new LoginPage(page);
-        const signUpPage = new SignUpPage(page);
-        const mail = new MailSlurpService();
-        const emailVerification = new EmailVerificationPage(page);
+        loginPage = new LoginPage(page);
+        signUpPage = new SignUpPage(page);
+        mail = new MailSlurpService();
+        emailVerification = new EmailVerificationPage(page);
 
+        // Create a new email inbox on MailSlurp 
         const inbox = await mail.createInbox();
+
+        // DPrint the generated new email from MailSlurp
         console.log('Generated Email: ', inbox.emailAddress);
 
+        // Navigate to Sodexo Local Sign In page
         await loginPage.goToLogin(`${generic.environment.stageURL}`);
 
+        // Navigate to Sign Up page
         const popupSignUpPage = await signUpPage.goToSignUpPage();
+
+        // Complete and submit the registration form
         await signUpPage.fillSignUpForm(popupSignUpPage, inbox.emailAddress);
 
+        // Get the Verification Email Link on MailSlurp
         const link = await mail.waitForVerificationEmail(inbox.id);
         console.log("Verification Link: ", link);
 
+        // Verify the Verification Email received from MailSlurp
         await emailVerification.verifyEmail(popupSignUpPage, link, inbox.emailAddress);
     });
 
